d0cf8330aa7fb3014000883a453326bd0226f1c7
G4me4u <G4me4u@gmail.com>
2018-01-15 17:57:06 +0100

    Added a simple timer class

---
+++ src/js/utils/timer.js
@@ -0,0 +1,75 @@
+
+class Timer {
+
+	constructor(tps, debug=false) {
+		this.tps = tps;
+		this.debug = debug;
+		
+		this.last = 0;
+		
+		this.missingTicks = 0;
+		
+		this.dt = 0.0;
+		this.ds = 0.0;
+		
+		this.ticks = 0;
+		this.frames = 0;
+	}
+
+	init() {
+		this.last = Date.now();
+		
+		this.missingTicks = 0;
+		
+		this.dt = 1.0;
+		this.ds = 0.0;
+		
+		this.ticks = 0;
+		this.frames = 0;
+	}
+	
+	clock() {
+		let now = Date.now();
+		let deltaS = (now - this.last) / 1000.0;
+		this.last = now;
+		
+		this.dt += deltaS * this.tps;
+		let ticksToGo = Math.floor(this.dt);
+		this.missingTicks += ticksToGo;
+		this.dt -= ticksToGo;
+		
+		if (this.debug) {
+			this.ds += deltaS;
+			if (this.ds >= 1.0) {
+				this.ds -= 1.0;
+				if (this.ds >= 1.0)
+					this.ds = 0;
+				
+				logger.info("Tps: " + this.ticks + " Fps: " + this.frames);
+				
+				this.ticks = 0;
+				this.frames = 0;
+			}
+		}
+	}
+
+	tickPassed() {
+		this.ticks++;
+		this.missingTicks--;
+	}
+	
+	framePassed() {
+		this.frames++;
+	}
+	
+	timeout(callback, minFps) {
+		const msPassed = Date.now() - this.last;
+
+		if (this.tps < minFps) {
+			minFps -= (minFps % this.tps);
+		} else minFps = this.tps;
+		
+		const msToSleep = Math.floor(1000 / minFps) - msPassed;
+		setTimeout(callback, msToSleep > 0 ? msToSleep : 0);
+	}
+}
