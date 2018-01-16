ab99fd15ef8a05c3c06b63c168200de49497cef4
G4me4u <G4me4u@gmail.com>
2018-01-16 10:17:35 +0100

    Rename project and code cleanup

--- src/js/minesweeperapp/ms_constants.js
+++ src/js/minesweeperapp/ms_constants.js
@@ -1,5 +1,5 @@
 const MS_CURSOR_TOGGLE = 16;
-const MS_NUM_BOMBS     = 16;
+const MS_NUM_BOMBS     =  8;
 
 const MS_TILE_COLOR = new Color(218, 218, 218);
 const MS_BOMB_COLOR = new Color( 64,  64,  64);
